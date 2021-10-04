70858bf3ff5b3adcbc8e295eb39199950bddeaf7
Chad Tarpey <chadtarpey@gmail.com>
2021-10-04 16:45:18 -0500

    created userActions.js

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,25 @@
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL } from "../constants/userConstants"
+import axios from "axios"
+
+
+export const register = (name, email, password) = async dispatch => {
+    try {
+        dispatch({
+            type: USER_REGISTER_REQUEST
+        })
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        const { data } = await axios.post("/api/users",
+            {
+                name, email, password
+            },
+            config)
+    } catch (error) {
+
+    }
+}
