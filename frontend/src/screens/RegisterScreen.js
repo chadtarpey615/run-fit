cbf92b1cf74a727a3f4a5cf9b16d8203e1c5568b
Chad Tarpey <chadtarpey@gmail.com>
2021-10-04 11:01:57 -0500

    created actions, constants, and reducers folder

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import { Link } from "react-router-dom"
 const RegisterScreen = () => {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
@@ -8,7 +8,7 @@ const RegisterScreen = () => {
 
     const registerUser = (e) => {
         e.preventDefault();
-        console.log(email)
+        console.log(email, password2)
     }
     return (
         <div>
@@ -16,7 +16,7 @@ const RegisterScreen = () => {
                 <h1>Please Sign up </h1>
                 <div className="email-input">
                     <label htmlFor="email">
-                        <input type="name" name="name" placeholder="Please enter your name" value={name} onChange={(e) => setEmail(e.target.value)} />
+                        <input type="name" name="name" placeholder="Please enter your name" value={name} onChange={(e) => setName(e.target.value)} />
                     </label>
                 </div>
                 <div className="email-input">
@@ -26,8 +26,17 @@ const RegisterScreen = () => {
                 </div>
                 <div className="email-input">
                     <label htmlFor="email">
-                        <input type="email" name="email" placeholder="Please enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input type="password" name="password2" placeholder="Please enter your password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </label>
+                </div>
+                <div className="email-input">
+                    <label htmlFor="email">
+                        <input type="password" name="password2" placeholder="Please verify your password" value={password2} onChange={(e) => setPassword2(e.target.value)} />
+                    </label>
+                </div>
+                <div className="submit-btn">
+                    <button type="submit">Register</button>
+                    <Link to="/">Have an account, click here to login.</Link>
                 </div>
             </form>
         </div>
