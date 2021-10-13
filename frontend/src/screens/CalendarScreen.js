0af8d2bac00e1f0ec5ab047cd63be90d6f27c2e1
Chad Tarpey <chadtarpey@gmail.com>
2021-10-13 16:39:32 -0500

    added onClick for calendar to add run event

--- frontend/src/screens/CalendarScreen.js
+++ frontend/src/screens/CalendarScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 import Tile from "../components/Tile"
@@ -6,16 +6,45 @@ import moment from 'moment'
 // import { useDispatch, useSelector } from "react-redux"
 
 const CalendarScreen = () => {
+
+    const [selectDay, setSelectDay] = useState(null)
+
+    const handleDay = (e) => {
+        setSelectDay(e)
+    }
+
     return (
         <>
-            {/* <h1 className="title">Run Fit Calendar</h1>
-            <div className="date">
-                {moment().format('MMMM ')}
-            </div>
-            <div className="blocks">
-                <Tile />
-            </div> */}
-            <DayPicker />
+
+            <DayPicker onDayClick={handleDay}
+
+            />
+            <button onClick={(e) => setSelectDay(null)}></button>
+            {selectDay ? (
+                <form className="run-form">
+                    <h1>Add run event form </h1>
+
+                    <div >
+                        <label htmlFor="name" > </label>
+                        <input type="text" placeholder="Add Event Name" />
+
+                    </div>
+                    <div >
+                        <label htmlFor="date" > </label>
+                        <input type="text" placeholder="Add Event Date" />
+
+                    </div>
+                    <div >
+                        <label htmlFor="length" > </label>
+                        <input type="text" placeholder="Add Event Total Distance" />
+
+                    </div>
+                    <div >
+                        <button>Enter Event</button>
+
+                    </div>
+                </form>
+            ) : <></>}
         </>
     )
 }
