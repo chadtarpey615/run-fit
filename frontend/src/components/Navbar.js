3c63d1ef22d1ad94d9c0928dfd8159f66e1b28df
Chad Tarpey <chadtarpey@gmail.com>
2021-11-22 15:07:11 -0600

    created a navbar.css file in the style folder

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import { logout } from "../actions/userActions"
 
+import "../style/navbar.css"
+
 const Navbar = () => {
     const dispatch = useDispatch()
     const userLogin = useSelector(state => state.userLogin)
@@ -20,18 +22,21 @@ const Navbar = () => {
             <nav className="navbar">
 
                 {userInfo ? (
-                    <>
+                    <div className="nav-links">
                         <li>Welcome {userInfo.data.name}</li>
-                        <li><Link to="/calendar">Home</Link></li>
-                        <li onClick={logoutHandler}><Link to="/login">Logout</Link></li>
 
-                    </>
+                        <ul className="nav-menu">
+                            <li><Link to="/calendar">Home</Link></li>
+                            <li onClick={logoutHandler}><Link to="/login">Logout</Link></li>
+                        </ul>
+                    </div>
                 ) :
-                    <>
-                        <li><Link to="/login">Login</Link></li>
-                        <li><Link to="/register">Sign Up</Link></li>
-
-                    </>
+                    <div className="nav-links">
+                        <ul className="nav-menu">
+                            <li><Link to="/login">Login</Link></li>
+                            <li><Link to="/register">Sign Up</Link></li>
+                        </ul>
+                    </div>
                 }
 
 
