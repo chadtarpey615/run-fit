4214ff80dbecbaba0b71ddd0defd1cdebc5d51c4
Chad Tarpey <chadtarpey@gmail.com>
2021-11-30 18:04:49 -0600

    changed home page styling

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+
+import "../style/homeScreen.css"
+import logo from "../style/download.jpeg"
+
+
 const HomeScreen = () => {
     return (
-        <div className="main-heading">
-            <header className="heading">
-                Welcome to Run-Fit
-            </header>
+        <>
+            <div className="main-heading">
+                <header className="heading">
+                    Welcome to Run-Fit
+                    <span className="header-line"></span>
+                    <p className="header-text">Where the running community comes together</p>
 
+                </header>
+                <div className="header-image">
+                    <img src={logo} alt="" />
+                </div>
+
+
+
+            </div>
             <div className="main-btn">
                 <Link to="/login"><button>LogIn</button></Link>
                 <Link to="/register" ><button>SignUp</button></Link>
             </div>
-        </div>
+        </>
     )
 }
 
