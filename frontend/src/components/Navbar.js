a221f77c2906d8e247cca8e5848d01dbb25331ff
Chad Tarpey <chadtarpey@gmail.com>
2021-12-04 15:15:59 -0600

    created a hamburger icon for mobile and still working on display when clicked

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import { logout } from "../actions/userActions"
@@ -6,12 +6,20 @@ import logo from "../style/images.jpeg"
 import "../style/navbar.css"
 
 const Navbar = () => {
+
     const dispatch = useDispatch()
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
     const logoutHandler = () => {
         dispatch(logout())
+    }
+
+    const openMenu = () => {
+        const menu = document.querySelector(".hamburger")
+        const icons = document.querySelector(".mobile-menu")
+        menu.classList.toggle("open")
+        icons.classList.remove("hidden")
     }
 
     // useEffect(() => {
@@ -31,11 +39,12 @@ const Navbar = () => {
 
                         <ul className="navbar-left">
                             <li>Welcome {userInfo.data.name || "User"}</li>
-                            <li><Link className="link" to="/calendar">Home</Link></li>
+                            {/* <li><Link className="link" to="/calendar">Home</Link></li> */}
                         </ul>
 
 
                         <ul className="navbar-right">
+                            <li><Link className="link" to="/calendar">Home</Link></li>
 
                             <li onClick={logoutHandler}><Link className="link" to="/login">Logout</Link></li>
                         </ul>
@@ -57,7 +66,18 @@ const Navbar = () => {
                     </div>
                 }
 
+                <div onClick={openMenu} className="hamburger">
+                    <span className="top"></span>
+                    <span className="middle"></span>
+                    <span className="bottom"></span>
+                </div>
+                <div className="mobile-menu hidden" id="menu">
+                    <ul>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/register">Sign Up</Link></li>
+                    </ul>
 
+                </div>
 
             </nav>
         </>
