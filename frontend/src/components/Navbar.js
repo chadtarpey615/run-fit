330d57b5415dca8ed54d071fd6f84b4d78bbf04a
Chad Tarpey <chadtarpey@gmail.com>
2021-12-05 15:42:26 -0600

    worked on side drawer for mobile hamburger nav button

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -19,7 +19,8 @@ const Navbar = () => {
         const menu = document.querySelector(".hamburger")
         const icons = document.querySelector(".mobile-menu")
         menu.classList.toggle("open")
-        icons.classList.remove("hidden")
+        icons.classList.toggle("hidden")
+        document.body.classList.toggle("no-scroll")
     }
 
     // useEffect(() => {
@@ -72,10 +73,19 @@ const Navbar = () => {
                     <span className="bottom"></span>
                 </div>
                 <div className="mobile-menu hidden" id="menu">
-                    <ul>
-                        <li><Link to="/login">Login</Link></li>
-                        <li><Link to="/register">Sign Up</Link></li>
-                    </ul>
+                    {userInfo ? (
+                        <ul className="mobile-links">
+                            <li><Link className="link" to="/calendar">Home</Link></li>
+
+                            <li onClick={logoutHandler}><Link className="link" to="/login">Logout</Link></li>
+                        </ul>
+                    ) : (
+                            <ul className="mobile-links">
+                                <li><Link className="link" to="/login">Login</Link></li>
+                                <li><Link className="link" to="/register">Sign Up</Link></li>
+                            </ul>
+                        )}
+
 
                 </div>
 
